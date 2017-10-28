c1c8f284488b44b40db7a24ea659a210bbd8d678
4419-3-2\user <fdsaf@fsk.net>
2017-10-28 10:30:34 +0800

    1.新增玩家射擊特效 2.新增註解

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -2,15 +2,15 @@
 
 public class PlayerShooting : MonoBehaviour
 {
-    public int damagePerShot = 20;
-    public float timeBetweenBullets = 0.15f;
+    public int damagePerShot = 20; //每次射擊給敵人的傷害
+    public float timeBetweenBullets = 0.15f; //間隔時間
     public float range = 100f;
 
 
     float timer;
     Ray shootRay = new Ray();
     RaycastHit shootHit;
-    int shootableMask;
+    int shootableMask; 
     ParticleSystem gunParticles;
     LineRenderer gunLine;
     AudioSource gunAudio;
@@ -30,9 +30,9 @@ public class PlayerShooting : MonoBehaviour
 
     void Update ()
     {
-        timer += Time.deltaTime;
+        timer += Time.deltaTime; 
 
-		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        if (Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0) //因為不希望玩家能一直發射 所以要大於 timeBetweenBullets 可攻擊的時間  Time.timeScale != 0 時間不是暫停
         {
             Shoot ();
         }
@@ -63,23 +63,23 @@ public class PlayerShooting : MonoBehaviour
         gunParticles.Play ();
 
         gunLine.enabled = true;
-        gunLine.SetPosition (0, transform.position);
+        gunLine.SetPosition (0, transform.position); //設位置在第0個 槍口的位置
 
         shootRay.origin = transform.position;
         shootRay.direction = transform.forward;
 
-        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask)) //Physics.Raycast模擬一個射線往前面打
         {
             EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
             if(enemyHealth != null)
             {
                 enemyHealth.TakeDamage (damagePerShot, shootHit.point);
             }
-            gunLine.SetPosition (1, shootHit.point);
+            gunLine.SetPosition (1, shootHit.point);//雷射槍的終點變成打到的位置
         }
         else
         {
-            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);//若沒有打到就把雷射槍的終點設在可打到的最大範圍
         }
     }
 }
