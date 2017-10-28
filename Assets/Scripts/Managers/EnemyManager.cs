49ebffbda4b6887c34b54504e46722766ccfcc18
4419-3-2\user <fdsaf@fsk.net>
2017-10-28 11:51:17 +0800

    1.新撐註解 2.新增zombear hellephant 3.新稱敵人的spawnpoint

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -10,19 +10,19 @@ public class EnemyManager : MonoBehaviour
 
     void Start ()
     {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);//InvokeRepeating  重複呼叫    每過幾秒 要呼叫的方法 第一次要delay多久時間(預設3秒)  每次間隔多久
     }
 
 
     void Spawn ()
     {
-        if(playerHealth.currentHealth <= 0f)
+        if(playerHealth.currentHealth <= 0f) //玩家的生命值小於等於0
         {
-            return;
+            return;  //返回 跳出這個spawn{}不會再執行 
         }
 
-        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);  //根據點隨機產生一個
 
-        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation); //如果玩家生命值不小於等於0  Instantiate複製產生一個敵人  位置和旋轉都放在槍口
     }
 }
